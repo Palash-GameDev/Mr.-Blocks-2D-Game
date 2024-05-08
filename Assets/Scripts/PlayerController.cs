c87647e000bc0d68b9588c4abc8dad0f6bc85494
Palash Choubisa <168923442+Palash-GameDev@users.noreply.github.com>
2024-05-09 03:16:29 +0530

    Added Enimies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D _rb2D;
     public float speed = 5f;
     public GameObject UiPanel;
+    public GameObject GameOverPanel;
     
     // Start is called before the first frame update
     void Start()
@@ -44,9 +46,15 @@ public class PlayerController : MonoBehaviour
     {
         if(other.tag == "Door")
         {
+            speed = 0;
             UiPanel.SetActive(true);
             Debug.Log("Player collided with " + other.name);
             Debug.Log("Level Completed!!!!!!!!!!!!!!!!!");
+        }
+        else if(other.tag == "Enemy")
+        {
+            GameOverPanel.SetActive(true);
+            speed = 0;
         }
         
     }
@@ -55,7 +63,9 @@ public class PlayerController : MonoBehaviour
     {
         UiPanel.SetActive(false);
         this.transform.position = new Vector3(0,0,0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
 
 
 }// Class end
